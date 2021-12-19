8d81b1da1247eb63dc31928d3ec421fb8ea33ae4
evgeniy-panferov <418vqftn8ApZ2J>
2021-12-19 11:40:31 +0300

    some change

--- src/test/java/com/actionservice/CouponServiceAdmitadApplicationTests.java
+++ src/test/java/com/actionservice/CouponServiceAdmitadApplicationTests.java
@@ -1,13 +1,13 @@
-package com.actionservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CouponServiceAdmitadApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.actionservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CouponServiceAdmitadApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
